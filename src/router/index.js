6293d964bd1b6b15c0aa4bedc7f86f5f60b58104
JDM-B550 <jendri@jdm-b550.home>
2020-09-14 15:52:41 -0400

    add login to index.js as '/'

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+//import Home from '../views/Home.vue'
 import Page2 from '../views/Page2.vue'
+import Login from '../views/Login.vue'
 
 Vue.use(VueRouter)
 
   const routes = [
   {
+    // This is our landing page
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Login',
+    component: Login
   },
   {
     path: '/page2',
